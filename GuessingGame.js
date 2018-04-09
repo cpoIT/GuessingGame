a3e435a9195087a1c4cbc0fa32069302c0293ff3
Kat Fries <katherynejf@gmail.com>
2018-04-09 09:25:55 -0400

    commit

---
+++ GuessingGame.js
@@ -0,0 +1,11 @@
+function generateWinningNumber() {
+  return Math.ceil(100*Math.random());
+}
+
+function shuffle (arr) {
+let unshuffledArr = arr.slice();
+for (let i = 0; i < arr.length; i++) {
+  let a
+}
+return retArr;
+}
